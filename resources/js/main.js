5c99862a38d4e4b61e8cdeaaedf4e6a740a0defa
Kandakov Yegor <yegorkandakov@gmail.com>
2017-12-09 09:49:57 +0200

    finished todo list and remove button

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,6 +1,67 @@
 document.getElementById('add').addEventListener('click', function () {
   var value = document.getElementById('item').value;
   if (value) {
-    
+    addTodoItem(value);
   }
+  document.getElementById('item').value = '';
 });
+
+document.getElementById('addImg').addEventListener('mouseover', function () {
+  this.src='resources/images/addHover.png';
+});
+
+document.getElementById('addImg').addEventListener('mouseout', function () {
+  this.src='resources/images/add.png';
+});
+
+function addTodoItem(text) {
+  var list = document.getElementById('todo');
+
+  var item = document.createElement('li');
+  item.innerText = text;
+
+  var buttons = document.createElement('div');
+  buttons.classList.add('buttons');
+
+  var remove = document.createElement('button');
+  remove.classList.add('remove');
+  remove.innerHTML = '<img id="deleteImg" src="resources/images/delete.png" />';
+  remove.addEventListener('click', removeItem);
+
+  var done = document.createElement('button');
+  done.classList.add('done');
+  done.innerHTML = '<img id="doneImg" src="resources/images/notDone.png" />';
+
+  buttons.appendChild(remove);
+  buttons.appendChild(done);
+  item.appendChild(buttons);
+  list.insertBefore(item, list.childNodes[0]);
+
+  addOnHoverListeners();
+}
+
+function removeItem() {
+  var item = this.parentNode.parentNode;
+  var ul = item.parentNode;
+  ul.removeChild(item);
+}
+
+function addOnHoverListeners() {
+
+  document.getElementById('deleteImg').addEventListener('mouseover', function () {
+    this.src='resources/images/deleted.png';
+  });
+
+  document.getElementById('deleteImg').addEventListener('mouseout', function () {
+    this.src='resources/images/delete.png';
+  });
+
+  document.getElementById('doneImg').addEventListener('mouseover', function () {
+    this.src='resources/images/done.png';
+  });
+
+  document.getElementById('doneImg').addEventListener('mouseout', function () {
+    this.src='resources/images/notDone.png';
+  });
+
+}
